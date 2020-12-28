dc826c8719bc4d728cf7018148ba641505ff1bea
Ed Gibbs <ed@edgibbs.com>
2020-12-27 22:24:29 -0800

    Get to green first at all cost

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -1,6 +1,13 @@
 defmodule GildedRoseTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "begin the journey of refactoring" do
+  describe "update_quality/1 with a backstage pass" do
+    test "returns sell_in and quality minus one" do
+      updated_items = GildedRose.update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
+      updated_item = List.first(updated_items)
+
+      assert updated_item.sell_in == 8
+      assert updated_item.quality == 3
+    end
   end
 end
