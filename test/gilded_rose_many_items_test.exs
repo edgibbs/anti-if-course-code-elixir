5b171a083aed602197b87527072fcdd29d512b7f
Ed Gibbs <ed@edgibbs.com>
2021-01-03 12:05:31 -0800

    Baseline as is functional test

--- test/gilded_rose_many_items_test.exs
+++ test/gilded_rose_many_items_test.exs
@@ -15,12 +15,12 @@ defmodule GildedRoseManyItemsTest do
       items = [
         %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 20},
         %Item{name: "Aged Brie", sell_in: 2, quality: 0},
-        %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7}
-        # %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80},
-        # %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: -1, quality: 80},
-        # %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20},
-        # %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 49},
-        # %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 49},
+        %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7},
+        %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80},
+        %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: -1, quality: 80},
+        %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20},
+        %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 49},
+        %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 49}
         # This Conjured item does not work properly yet
         # %Item{name: "Conjured Mana Cake", sell_in: 3, quality: 6},
       ]
@@ -41,7 +41,12 @@ defmodule GildedRoseManyItemsTest do
       assert day_2_items == [
                "+5 Dexterity Vest 9 19",
                "Aged Brie 1 1",
-               "Elixir of the Mongoose 4 6"
+               "Elixir of the Mongoose 4 6",
+               "Sulfuras, Hand of Ragnaros 0 80",
+               "Sulfuras, Hand of Ragnaros -1 80",
+               "Backstage passes to a TAFKAL80ETC concert 14 21",
+               "Backstage passes to a TAFKAL80ETC concert 9 50",
+               "Backstage passes to a TAFKAL80ETC concert 4 50"
              ]
 
       # Day 3
@@ -54,7 +59,12 @@ defmodule GildedRoseManyItemsTest do
       assert day_3_items == [
                "+5 Dexterity Vest 8 18",
                "Aged Brie 0 2",
-               "Elixir of the Mongoose 3 5"
+               "Elixir of the Mongoose 3 5",
+               "Sulfuras, Hand of Ragnaros 0 80",
+               "Sulfuras, Hand of Ragnaros -1 80",
+               "Backstage passes to a TAFKAL80ETC concert 13 22",
+               "Backstage passes to a TAFKAL80ETC concert 8 50",
+               "Backstage passes to a TAFKAL80ETC concert 3 50"
              ]
     end
   end
