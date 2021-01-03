4b50fbcf517887a794016e93bc439280418edad8
Ed Gibbs <ed@edgibbs.com>
2021-01-03 12:01:32 -0800

    Got baseline with 3 items working

--- test/gilded_rose_many_items_test.exs
+++ test/gilded_rose_many_items_test.exs
@@ -1,7 +1,6 @@
 defmodule TestStuff do
   def get_item_lines(some_items) do
     Enum.reduce(some_items, [], fn item, acc ->
-      # IO.puts("Items in the items line loop: #{inspect(some_items)}")
       [acc] ++ ["#{item.name} #{item.sell_in} #{item.quality}"]
     end)
   end
@@ -13,34 +12,50 @@ defmodule GildedRoseManyItemsTest do
 
   describe "update_quality/1 with many items" do
     test "returns the expected results for now" do
-      items =  [
-          %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 20},
-          %Item{name: "Aged Brie", sell_in: 2, quality: 0},
-          # %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7},
-          # %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80},
-          # %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: -1, quality: 80},
-          # %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20},
-          # %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 49},
-          # %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 49},
-          # This Conjured item does not work properly yet
-          # %Item{name: "Conjured Mana Cake", sell_in: 3, quality: 6},
-          ]
+      items = [
+        %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 20},
+        %Item{name: "Aged Brie", sell_in: 2, quality: 0},
+        %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7}
+        # %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80},
+        # %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: -1, quality: 80},
+        # %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20},
+        # %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 49},
+        # %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 49},
+        # This Conjured item does not work properly yet
+        # %Item{name: "Conjured Mana Cake", sell_in: 3, quality: 6},
+      ]
 
+      number_of_items = Enum.count(items)
       report_lines = []
 
-      # Day 0
-      report_lines = report_lines ++ ["-- day 1 --"] ++ get_item_lines(items)
-      IO.puts "report lines is #{inspect(report_lines)}"
-
       # Day 1
-      items = GildedRose.update_quality(items)
-      report_lines = report_lines ++ ["-- day 2 --"] ++ get_item_lines(items)
-      IO.puts "report lines is #{inspect(report_lines)}"
+      report_lines = report_lines ++ ["-- day 1 --"] ++ get_item_lines(items)
 
       # Day 2
       items = GildedRose.update_quality(items)
+
+      report_lines = report_lines ++ ["-- day 2 --"] ++ get_item_lines(items)
+      index = Enum.find_index(report_lines, fn item -> item == "-- day 2 --" end)
+      day_2_items = Enum.slice(report_lines, index + 1, number_of_items) |> List.flatten()
+
+      assert day_2_items == [
+               "+5 Dexterity Vest 9 19",
+               "Aged Brie 1 1",
+               "Elixir of the Mongoose 4 6"
+             ]
+
+      # Day 3
+      items = GildedRose.update_quality(items)
+
       report_lines = report_lines ++ ["-- day 3 --"] ++ get_item_lines(items)
-      IO.puts "report lines is #{inspect(report_lines)}"
+      index = Enum.find_index(report_lines, fn item -> item == "-- day 3 --" end)
+      day_3_items = Enum.slice(report_lines, index + 1, number_of_items) |> List.flatten()
+
+      assert day_3_items == [
+               "+5 Dexterity Vest 8 18",
+               "Aged Brie 0 2",
+               "Elixir of the Mongoose 3 5"
+             ]
     end
   end
 end
